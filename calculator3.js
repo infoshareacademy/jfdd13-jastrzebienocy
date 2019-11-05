051d1c6c2b7a74307282e02a58520681aa70ac74
Kruchy1980 <jakub.kruszynski@vp.pl>
2019-11-05 11:05:51 +0100

    Modal window with calculator added

--- calculator3.js
+++ calculator3.js
@@ -1,3 +1,23 @@
+// Modal window show :)
+let modal = document.querySelector(".modal");
+let trigger = document.querySelector(".trigger");
+let closeButton = document.querySelector(".close-button");
+
+function toggleModal() {
+  modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+  if (event.target === modal) {
+    toggleModal();
+  }
+}
+
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
+
+
 // Variables:
 // Account quantity prices
 const priceA = 99;
