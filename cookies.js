9418aa4f5d50af24245db007573877a09a712eec
Filip Siwiec <filipsiwiec@gmal.com>
2019-11-08 22:46:33 +0100

    cookies finished

--- cookies.js
+++ cookies.js
@@ -1,70 +1,34 @@
-// const banner = document.querySelector('.cookie-banner')
-// const guzik = document.querySelector('.cookie-button')
+const banner = document.querySelector('.cookie-banner')
+const cookieButton = document.querySelector('.cookie-button')
 
-
-// let setCookie = () => {
-//     document.cookie ="name=user;expires=Fri, 31 Dec 9999 23:59:59 GMT;path=/;SameSite=Lax";
-// };
-
-// function cookieEvent () {
-//     if ( document.cookie === true ) { banner.style.display = "none" } 
-//     else { banner.style.display =  "block"; }
-// };
-
-// guzik.addEventListener('click',function(){
-//     banner.style.display = "none";
-//     setCookie ();
-// });
-
-// cookieEvent();
-
-
-
-const banner = document.querySelector('.cookie-banner');
-const guzik = document.querySelector('.cookie-button');
 let setCookie = () => {
-    document.cookie = `name=user;expires=Fri, 31 Dec 9999 23:59:59 GMT;path=/;SameSite=Lax`;
+   document.cookie ="name=user;expires=Fri, 31 Dec 9999 23:59:59 GMT;path=/;SameSite=Lax";
 };
 
-function cookieEvent() {
-    if (document.cookie === true) {
-        banner.style.display = 'none'
-    } else {
-        banner.style.display = 'block';
-    }
+function getCookie(cname) {
+   const name = cname + "=";
+   const decodedCookie = decodeURIComponent(document.cookie);
+   const ca = decodedCookie.split(';');
+   for(let i = 0; i <ca.length; i++) {
+       let c = ca[i];
+       while (c.charAt(0) === ' ') {
+           c = c.substring(1);
+       }
+       if (c.indexOf(name) === 0) {
+           return c.substring(name.length, c.length);
+       }
+   }
+   return "";
 }
 
-guzik.addEventListener('click', function () {
-    banner.style.display = 'none';
-    setCookie();
+function cookieEvent () {
+   if ( getCookie ('name')=== '' ) { banner.style = 'display: block' }
+};
+   cookieButton.addEventListener('click',function(){
+   banner.style = 'display: none';
+   setCookie ();
 });
+
 cookieEvent();
 
-
-
-
-
-
-
-// function getCookie(cname) {
-//     const name = cname + "=";
-//     const decodedCookie = decodeURIComponent(document.cookie);
-//     const ca = decodedCookie.split(';');
-//     for(let i = 0; i <ca.length; i++) {
-//         let c = ca[i];
-//         while (c.charAt(0) === ' ') {
-//             c = c.substring(1);
-//         }
-//         if (c.indexOf(name) === 0) {
-//             return c.substring(name.length, c.length);
-//         }
-//     }
-//     return "";
-// }
-
-// const testing = () => {
-//     if (getCookie('name') === '') 
-//     { banner.style = 'display: block'}}
-
-//     testing()
-    // getCookie()
+    
