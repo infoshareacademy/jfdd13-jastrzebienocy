afe2fa9346a23ca9011f9762f1d1fc2f1a0287c3
Adam Toczko <adamtoczko80@gmail.com>
2019-11-10 10:39:24 +0100

    small changes, trying with navigation hoghlight

--- navigation.js
+++ navigation.js
@@ -32,3 +32,46 @@ function toggleMenu() {
   window.onscroll = function () {
     navigationBehavior();
     };
+
+    function onScroll(event){
+      var sections = document.querySelectorAll(".section");
+      var scrollPos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+      
+      for( var i = 0; i < sections.length; i++ ){
+        var currLink = sections[i]; 
+        var val = currLink.getAttribute('href');
+        var refElement = document.querySelector(val);
+          if( refElement.offsetTop <= scrollPos && ( refElement.offsetTop + refElement.offsetHeight > scrollPos)){
+            document.querySelector('#menu-center ul li a').classList.remove('active');
+            currLink.classList.add('active');
+          }else{
+             currLink.classList.remove('active');
+           }
+      }   
+    };
+    
+    window.document.addEventListener('scroll', onScroll );
+
+    // jak zrobic podswietlanie //
+    // (function() {
+    //   'use strict';
+    
+    //   var section = document.querySelectorAll(".section");
+    //   var sections = {};
+    //   var i = 0;
+    
+    //   Array.prototype.forEach.call(section, function(e) {
+    //     sections[e.id] = e.offsetTop;
+    //   });
+    
+    //   window.onscroll = function() {
+    //     var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+    
+    //     for (i in sections) {
+    //       if (sections[i] <= scrollPosition) {
+    //         document.querySelector('.active').setAttribute('class', ' ');
+    //         document.querySelector('a[href*=' + i + ']').setAttribute('class', 'active');
+    //       }
+    //     }
+    //   };
+    // })();
