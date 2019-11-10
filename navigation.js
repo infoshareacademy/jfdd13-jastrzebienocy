26b6c59a5653a47ea477886c9da6cff77c568730
Adam Toczko <adamtoczko80@gmail.com>
2019-11-10 17:25:23 +0100

    nav

--- navigation.js
+++ navigation.js
@@ -38,7 +38,7 @@ function toggleMenu() {
       function sectionsHighlight() {
           const section = document.querySelectorAll(".section");
           const sections = {};
-          leti = 0;
+          let i = 0;
         
           Array.prototype.forEach.call(section, function(e) {
             sections[e.id] = e.offsetTop;
@@ -62,58 +62,6 @@ function toggleMenu() {
 
 
 
-
-
-
-
-
-
-
-
-    // offsetTop: 807
-    // offsetTop: 1449
-    // offsetTop: 2546
-    // offsetTop: 2925
-    // function event){
-    //   var sections = document.querySelectorAll(".section");  JEST DOBRE 
-    // var scrollPos = window.pageYOffset || document.documentElement.scrollTop ; JEST DOBRE 
-    //   for( var i = 0; i < sections.length; i++ ){
-    //     var currLink = sections[i]; 
-    //     var val = document.querySelector("#myLinks > a");
-    //     // var refElement = document.querySelector(val);
-    //       if( val.offsetTop <= scrollPos && ( val.offsetTop + val.offsetHeight > scrollPos)){
-    //         document.document.querySelector("#myLinks > a").classList.remove('active');
-    //         currLink.classList.add('active');
-    //       }else{
-    //          currLink.classList.remove('active');
-    //        }
-    //   }   
-    // };
-    
-    // window.document.addEventListener('scroll', onScroll );
-    
-    // jak zrobic podswietlanie //
-    // (function() {
-    
-    //   var section = document.querySelectorAll(".section");
-    //   var sections = {};
-    //   var i = 0;
-    
-    //   Array.prototype.forEach.call(section, function(e) {
-    //     sections[e.id] = e.offsetTop;
-    //   });
-    
-    //   window.onscroll = function() {
-    //     var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
-    
-    //     for (i in sections) {
-    //       if (sections[i] <= scrollPosition) {
-    //         document.querySelector('.active').setAttribute('class', ' ');
-    //         document.querySelector('a[href*=' + i + ']').setAttribute('class', 'active');
-    //       }
-    //     }
-    //   };
-    // })();
 
 
 
