384ed9a222e4142ca2054bcac9e09e00555176a7
Kinga <zawarczynska.ko@gmail.com>
2019-11-11 20:46:36 +0200

    working game

--- game-script.js
+++ game-script.js
@@ -1,6 +1,7 @@
 const InnerInstButton = document.querySelector("#instruction");
 const overlay = document.querySelector(".game-instruction-overlay");
 const InsBox = document.querySelector(".game-instruction-box");
+
 const playerWidth = 160;
 const fruitWidth = 30;
 const playerHeight = 110;
@@ -8,20 +9,23 @@ const fruitHeight = 30;
 const oneMove = 10;
 const domContainer = document.querySelector(".container");
 const domRectagle = document.querySelector(".human");
+
 let falling;
 let newFruitInterval;
-let intervalOfFalling = 50;
-let intervalOfNewFruit = 3000;
-let minSpeed = 3;
+let intervalOfFalling;
+let intervalOfNewFruit;
+let minSpeed;
 let check = false;
+
 
 class Player {
   constructor(human) {
     (this.x = human.offsetLeft),
-      (this.y = human.offsetTop),
-      (this.points = 0),
-      (this.lifes = 5);
+    (this.y = human.offsetTop),
+    this.points = 0,
+    this.lifes = 5;
   }
+
   left() {
     this.x -= oneMove;
   }
@@ -31,22 +35,23 @@ class Player {
   down() {
     this.y += oneMove;
   }
-
+ 
   addPoint() {
     this.points += 1;
-    document.querySelector(".counterPlus").innerHTML = this.points;
+    document.querySelector('.counterPlus').innerHTML = this.points;
 
     if (this.points >= 7) {
       intervalOfFalling = 15;
       intervalOfNewFruit = 1000;
       minSpeed = 7;
-    } else if (this.points >= 15) {
+    } else if (this.points >=15){
       intervalOfFalling = 10;
       intervalOfNewFruit = 500;
-      minSpeed = 20;
+      minSpeed = 15;
     }
   }
 }
+
 class Fruit {
   constructor(fruit, speed) {
     this.domFruit = fruit;
@@ -55,21 +60,24 @@ class Fruit {
     this.y = fruit.offsetTop;
     this.firstTouch = false;
   }
+
   down() {
     this.y += this.speed;
   }
 }
+
 class MoveFruits {
   static RenderElement(Fruit, domFruit) {
     domFruit.style.top = Fruit.y + "px";
     domFruit.style.left = Fruit.x + "px";
   }
+
   static move(fruit, domFruit) {
     if (fruit.y > 470) {
       domFruit.style.backgroundImage = "url(./gameIMG/lost.png)";
-      domFruit.style.height = "80px";
-      domFruit.style.width = "80px";
-
+      domFruit.style.height = '80px'
+      domFruit.style.width = '80px'
+      
       setTimeout(() => {
         domFruit.remove();
       }, 1000);
@@ -80,71 +88,69 @@ class MoveFruits {
     }
   }
 }
+
 class Move {
   static RenderElement(player, ludzik) {
     ludzik.style.left = player.x + "px";
     ludzik.style.top = player.y + "px";
   }
+
   static move(event, player1) {
     if (event.key === "ArrowLeft" && player1.x > 0) {
       player1.left();
       Move.RenderElement(player1, domRectagle);
-    } else if (
-      event.key === "ArrowRight" &&
-      player1.x < domContainer.offsetWidth - playerWidth
-    ) {
+    } else if (event.key === "ArrowRight" && player1.x < domContainer.offsetWidth - playerWidth ) {
       player1.right();
       Move.RenderElement(player1, domRectagle);
     }
 
-    if (player1.x >= domContainer.offsetWidth) {
+    if (player1.x >= domContainer.offsetWidth) { 
       Move.RenderElement(player1, domRectagle);
-    } else if (player1.x <= 0) {
+    } else if (player1.x <= 0 ) {
       Move.RenderElement(player1, domRectagle);
     }
   }
 }
+
 const player1 = new Player(domRectagle);
 document.addEventListener("keydown", event => Move.move(event, player1));
-function start() {
-  document.querySelector(".human").style.opacity = "1";
-  document.querySelector(".points").style.opacity = "1";
-  document.getElementById("end-game").classList.remove("shown");
-  document.getElementById("score").classList.remove("shown");
-  document.getElementById("score").innerText = " ";
-  minSpeed = 3;
-  intervalOfFalling = 50;
-  intervalOfNewFruit = 3000;
-  let pointsToShow = parseInt(localStorage.getItem("playerPoints"));
 
+function start() {
+  document.querySelector('.human').style.opacity = '1';
+  document.querySelector('.points').style.opacity = '1';
+  document.getElementById('end-game').classList.remove('shown');
+  document.getElementById('score').classList.remove('shown');
+  document.getElementById('score').innerText = ' ';
   minSpeed = 3;
   intervalOfFalling = 50;
   intervalOfNewFruit = 3000;
-  let pointsToShow = parseInt(localStorage.getItem("playerPoints"));
+  let pointsToShow = parseInt(localStorage.getItem('playerPoints'));
 
   clearInterval(falling);
   clearInterval(newFruitInterval);
-  player1.points = 0;
-  document.querySelector(".counterPlus").innerHTML = player1.points;
 
-  let fruitsFromDom = document.querySelectorAll(".fruit");
+  player1.points = 0;
+  document.querySelector('.counterPlus').innerHTML = player1.points;
+
+  let fruitsFromDom = document.querySelectorAll('.fruit')
   fruitsFromDom.forEach(element => element.remove());
   let fruits = [];
 
-  let counterMinus = document.querySelector(".counterMinus");
+  let counterMinus = document.querySelector('.counterMinus')
   counterMinus.innerHTML = `
     <div class="life"><i class="fas fa-heart"></i></div>
     <div class="life"><i class="fas fa-heart"></i></div>
     <div class="life"><i class="fas fa-heart"></i></div>
     <div class="life"><i class="fas fa-heart"></i></div>
     <div class="life"><i class="fas fa-heart"></i></div>
-  `;
+  `
 
-  let lifes = document.querySelectorAll(".life");
+  let lifes = document.querySelectorAll('.life');
 
   newFruitInterval = setInterval(() => {
     const newDomFruit = document.createElement("div");
     newDomFruit.classList.add("fruit");
+
     if (Math.random() <= 0.25) {
       newDomFruit.classList.add("banana");
     } else if (Math.random() > 0.25 && Math.random() < 0.5) {
@@ -159,6 +165,8 @@ function start() {
     fruits.push(newFruit);
   }, intervalOfNewFruit);
 
+
+      
   function checkCollision(fruits, player) {
     for (let i = 0; i < fruits.length; i++) {
       const fruit = fruits[i];
@@ -169,74 +177,78 @@ function start() {
       let bottomEdgeFruit = fruit.y + fruitHeight;
       let topEdgePlayer = player.y;
       let bottomEdgePlayer = player.y + playerHeight;
-      let record = document.querySelector(".counterPlus").innerText;
-      let playerName = localStorage.getItem("name");
-      localStorage.setItem("playerPoints", record);
-      let highscore = localStorage.getItem("highScore");
+      let record = document.querySelector('.counterPlus').innerText;
+      let playerName = localStorage.getItem('name');
+      localStorage.setItem('playerPoints', record);
+      let highscore = localStorage.getItem('highScore');
 
-      if (
-        ((leftEdgePplayer <= leftEdgeFruit &&
-          leftEdgeFruit <= rightEdgePlayer) ||
-          (leftEdgePplayer <= rightEdgeFruit &&
-            rightEdgeFruit <= rightEdgePlayer)) &&
-        (bottomEdgeFruit >= topEdgePlayer &&
-          bottomEdgeFruit <= bottomEdgePlayer) &&
-        !fruit.firstTouch
+
+      if ((
+        (((leftEdgePplayer <= leftEdgeFruit) && (leftEdgeFruit <= rightEdgePlayer)) ||
+          ((leftEdgePplayer <= rightEdgeFruit) && (rightEdgeFruit <= rightEdgePlayer))) &&
+        ((bottomEdgeFruit >= topEdgePlayer) && (bottomEdgeFruit <= bottomEdgePlayer)))
+        && !fruit.firstTouch
       ) {
         fruit.firstTouch = true;
         fruit.domFruit.remove();
         player.addPoint();
-      } else if (bottomEdgeFruit >= 500 && !fruit.firstTouch) {
+      }
+      else if (bottomEdgeFruit >= 500 && !fruit.firstTouch) {
         fruit.firstTouch = true;
-        lifes[lifes.length - 1].remove();
-        lifes = document.querySelectorAll(".life");
+        lifes[lifes.length - 1].remove()
+        lifes = document.querySelectorAll('.life')
         if (lifes.length == 0 && check == true) {
           clearInterval(falling);
           clearInterval(newFruitInterval);
-
-          document.querySelector(".points").style.opacity = "0";
-          document.getElementById("end-game").classList.add("shown");
-          document.getElementById("end-game").classList.add("cat");
-
+          
+          document.querySelector('.points').style.opacity = '0';
+          document.getElementById('end-game').classList.add('shown');
+          document.getElementById('end-game').classList.add('cat');
+         
           setTimeout(() => {
-            document.getElementById("score").classList.add("shown");
-
+            document.getElementById('score').classList.add('shown');
+            
             if (highscore === null) {
-              localStorage.setItem("highScore", pointsToShow);
-              highscore = localStorage.getItem("highScore");
+              localStorage.setItem('highScore', pointsToShow)
+              highscore = localStorage.getItem('highScore')
+              
             }
             if (highscore < pointsToShow) {
-              localStorage.setItem("highScore", pointsToShow);
-              highest = localStorage.getItem("highScore");
-              document.getElementById("score").classList.add("highscore");
-              document.querySelector(".highscore").innerText = `${playerName}
+              localStorage.setItem('highScore', pointsToShow)
+              highest = localStorage.getItem('highScore')
+              document.getElementById('score').classList.add('highscore');
+              document.querySelector('.highscore').innerText = `${playerName}
               to Twój rekord!
               Zdobywasz ${pointsToShow} punktów!`;
+
             } else if (highscore >= pointsToShow) {
-              document.getElementById("score").classList.add("yourscore");
-              document.querySelector(".yourscore").innerText = `${playerName} 
+              document.getElementById('score').classList.add('yourscore');
+              document.querySelector('.yourscore').innerText = `${playerName} 
               zdobywasz ${pointsToShow} punktów`;
+             
             }
           }, 3400);
-          document.getElementById("startGame").innerText = "Jeszcze raz!";
+          document.getElementById('startGame').innerText = 'Jeszcze raz!'
         }
       }
-    }
+    };
   }
+
   falling = setInterval(() => {
     fruits.forEach(fruit => {
       MoveFruits.move(fruit, fruit.domFruit);
     });
-    checkCollision(fruits, player1);
+    checkCollision(fruits, player1)
   }, intervalOfFalling);
+ 
 }
 
-const startGameBtn = document.querySelector("#startGame");
-startGameBtn.addEventListener("click", start);
+const startGameBtn = document.querySelector('#startGame');
+startGameBtn.addEventListener('click', start);
 
-function InstInner() {
+function InstInner () {
   overlay.style.display = "block";
   InsBox.style.display = "block";
-}
+};
 
 InnerInstButton.addEventListener("click", InstInner);
