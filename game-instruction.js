4ee31730f71be08ce48ce02d6ff32243f37a5069
Kruchy1980 <jakub.kruszynski@vp.pl>
2019-11-11 13:21:20 +0100

    Small corrections to game instruction with name validation added

--- game-instruction.js
+++ game-instruction.js
@@ -8,17 +8,18 @@ const InsBox = document.querySelector(".game-instruction-box");
 
 // Regular expressions for validating email before the game instruction shows itself
 // Name Validator
-let checkName = /[a-z\bąćśńółężź]$/i;
+let checkName = /[a-z\bąćśńółężź]{3,}$/i;
 // Email validator
-let checkEmail = /^[a-z\d]{3,20}[\w\d.-]*@(?:[a-z\d]+[a-z\d-]+\.){1,5}[a-z]{2,6}$/i;
+let checkEmail = /^[a-z\d]{3,}[\w\d.-]*@(?:[a-z\d]+[a-z\d-]+\.){1,5}[a-z]{2,6}$/i;
 
 // Function which checks the form validation b4 let you play the game (b4 read instruction)
 function checkValidation() {
   if (!checkName.test(name.value))
     alert(
-      "Musisz podać swoje imię nie używając spacji czy innych znaków łączeń"
+      "Musisz podać swoje imię nie używając spacji czy innych znaków łączeń - minimalna ilość znaków to 3."
     );
-  else if (!checkEmail.test(email.value)) alert("Wpisz poprawny adres email");
+  else if (!checkEmail.test(email.value))
+    alert("Wpisz poprawny adres email minimalna długość znaków to 3.");
   else {
     localStorage.setItem("player:", name.value);
     Instruction();
