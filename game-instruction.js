7062cec5687d9222e799be467a0595dc972076bf
Kruchy1980 <jakub.kruszynski@vp.pl>
2019-11-12 19:42:13 +0100

    Validation email corrected

--- game-instruction.js
+++ game-instruction.js
@@ -1,33 +1,29 @@
+let name = document.querySelector('#name')
 
-let name = document.querySelector("#name");
+let email = document.querySelector('#email')
+const sendButton = document.querySelector('.send')
 
-let email = document.querySelector("#email");
-const sendButton = document.querySelector(".send");
+const overlay = document.querySelector('.game-instruction-overlay')
+const InsBox = document.querySelector('.game-instruction-box')
 
-const overlay = document.querySelector(".game-instruction-overlay");
-const InsBox = document.querySelector(".game-instruction-box");
+let checkName = /[a-z\bąćśńółężź]{3,}$/i
 
-let checkName = /[a-z\bąćśńółężź]{3,}$/i;
+let checkEmail = /^[a-z\d]{3,20}[\w\d.-]*@(?:[a-z\d]+[a-z\d-]+\.){1,}[a-z]{2,}$/i
 
-let checkEmail = /^[a-z\d]{3,20}[\w\d.-]*@(?:[a-z\d]+[a-z\d-]+\.){1,5}[a-z]{2,6}$/i;
-
-
-function checkValidation() {
-  if (!checkName.test(name.value))
+function checkValidation () {
+  if (!checkName.test(name.value)) {
     alert(
-      "Musisz podać swoje imię nie używając spacji czy innych znaków łączeń, minimum trzy litery."
-    );
-  else if (!checkEmail.test(email.value))
-    alert("Wpisz poprawny adres email nazwa min trzy litery");
-  else {
-    localStorage.setItem("name", name.value);
-    Instruction();
+      'Musisz podać swoje imię nie używając spacji czy innych znaków łączeń, minimum trzy litery.'
+    )
+  } else if (!checkEmail.test(email.value)) { alert('Wpisz poprawny adres email nazwa min trzy litery') } else {
+    localStorage.setItem('name', name.value)
+    Instruction()
   }
 }
 
 const Instruction = () => {
-  overlay.style.display = "block";
-  InsBox.style.display = "block";
-};
+  overlay.style.display = 'block'
+  InsBox.style.display = 'block'
+}
 
-sendButton.addEventListener("click", checkValidation);
+sendButton.addEventListener('click', checkValidation)
