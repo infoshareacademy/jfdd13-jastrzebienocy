af210ead60b0fbcfa44fbe86a549e369059d3eb2
Kruchy1980 <jakub.kruszynski@vp.pl>
2019-11-12 19:50:48 +0100

    Update

--- game-instruction.js
+++ game-instruction.js
@@ -8,14 +8,16 @@ const InsBox = document.querySelector('.game-instruction-box')
 
 let checkName = /[a-z\bąćśńółężź]{3,}$/i
 
-let checkEmail = /^[a-z\d]{3,20}[\w\d.-]*@(?:[a-z\d]+[a-z\d-]+\.){1,}[a-z]{2,}$/i
+let checkEmail = /^[a-z\d]{3,}[\w\d.-]*@(?:[a-z\d]+[a-z\d-]+\.){1,}[a-z]{2,}$/i
 
 function checkValidation () {
   if (!checkName.test(name.value)) {
     alert(
       'Musisz podać swoje imię nie używając spacji czy innych znaków łączeń, minimum trzy litery.'
     )
-  } else if (!checkEmail.test(email.value)) { alert('Wpisz poprawny adres email nazwa min trzy litery') } else {
+  } else if (!checkEmail.test(email.value)) {
+    alert('Wpisz poprawny adres email nazwa min trzy litery')
+  } else {
     localStorage.setItem('name', name.value)
     Instruction()
   }
